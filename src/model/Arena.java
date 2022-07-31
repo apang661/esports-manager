9fb914b356dca6ec0a15e84f47bffe4d7d872778
ryn5 <ryanmtan5@gmail.com>
2022-07-30 18:34:49 -0700

    Implement Arena model class

--- src/model/Arena.java
+++ src/model/Arena.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Arena {
+    private final int aID;
+    private final String name;
+    private final String city;
 
+    public Arena(int aID, String name, String city) {
+        this.aID = aID;
+        this.name = name;
+        this.city = city;
+    }
+
+    public int getaID() {
+        return aID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
 }
