9efa8df707a097b5d76064d5668da56ab3aedb31
ryn5 <ryanmtan5@gmail.com>
2022-08-02 02:37:53 -0700

    Add panels to ViewerScreen

--- src/ui/ViewerScreen.java
+++ src/ui/ViewerScreen.java
@@ -1,14 +1,56 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ViewerScreen extends AbstractScreen {
     public ViewerScreen(int viewerID) {
         super();
+        addTab("Account", setupAccountPanel());
         addTab("Tickets", setupTicketsPanel());
+        addTab("Schedule", setupSchedulePanel());
+        addTab("Standings", setupStandingsPanel());
+        addTab("Players", setupPlayersPanel());
+
+    }
+
+    private JPanel setupAccountPanel() {
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.RED);
+        JLabel test = new JLabel("test");
+        panel.add(test);
+        return panel;
     }
 
     private JPanel setupTicketsPanel() {
-        return new JPanel();
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.RED);
+        JLabel test = new JLabel("test");
+        panel.add(test);
+        return panel;
+    }
+
+    private JPanel setupSchedulePanel() {
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.RED);
+        JLabel test = new JLabel("test");
+        panel.add(test);
+        return panel;
+    }
+
+    private JPanel setupStandingsPanel() {
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.RED);
+        JLabel test = new JLabel("test");
+        panel.add(test);
+        return panel;
+    }
+
+    private JPanel setupPlayersPanel() {
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.RED);
+        JLabel test = new JLabel("test");
+        panel.add(test);
+        return panel;
     }
 }
