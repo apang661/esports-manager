be7d4351587ebc16e5faffa1045e7f225233c474
ryn5 <ryanmtan5@gmail.com>
2022-08-02 21:21:52 -0700

    Add toolbar to ViewerTicketsPanel

--- src/tabs/ViewerTicketsPanel.java
+++ src/tabs/ViewerTicketsPanel.java
@@ -1,39 +1,56 @@
 package tabs;
 
+import model.Ticket;
+import popUps.buyTicket;
 import ui.AbstractScreen;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static ui.AbstractScreen.SCREEN_WIDTH;
+
 public class ViewerTicketsPanel extends Panel {
+
+    private Ticket selectedTicket;
+
     public ViewerTicketsPanel(AbstractScreen parent) {
         super(parent);
     }
 
-//    private void addBottom() {
-//        JPanel bottom = new JPanel(new BorderLayout());
-//        AbstractScreen.setColors(bottom, "s");
-//        JPanel tools = new JPanel(new GridLayout(0, 1));
-//        AbstractScreen.setColors(tools, "s");
-//        JButton deleteGame = new JButton("Delete Game");
-//        deleteGame.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                deleteGame();
-//            }
-//        });
-//        deleteGame.setPreferredSize(new Dimension(SCREEN_WIDTH * 3/4, 50));
-//        AbstractScreen.setColors(deleteGame, "s");
-//        JButton addGame = new JButton("Add Game");
-//        addGame.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                addGame();
-//            }
-//        });
-//        addGame.setPreferredSize(new Dimension(SCREEN_WIDTH * 3/4, 50));
-//        AbstractScreen.setColors(addGame, "s");
-//        bottom.add(displayGames(), BorderLayout.NORTH);
-//        tools.add(deleteGame);
-//        tools.add(addGame);
-//        bottom.add(tools, BorderLayout.SOUTH);
-//        panel.add(bottom, BorderLayout.SOUTH);
-//    }
+    private void addToolbar() {
+        JPanel tools = new JPanel(new GridLayout(0, 1));
+        AbstractScreen.setColors(tools, "s");
+        JButton buyTicketButton = new JButton("Buy Ticket");
+        buyTicketButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buyTicket();
+            }
+        });
+        JButton refundTicketButton = new JButton("Refund Ticket");
+        refundTicketButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                refundTicket();
+            }
+        });
+        buyTicketButton.setPreferredSize(new Dimension(SCREEN_WIDTH * 3/4, 50));
+        AbstractScreen.setColors(buyTicketButton, "s");
+        refundTicketButton.setPreferredSize(new Dimension(SCREEN_WIDTH * 3/4, 50));
+        AbstractScreen.setColors(refundTicketButton, "s");
+        tools.add(buyTicketButton);
+        tools.add(refundTicketButton);
+        panel.add(tools, BorderLayout.SOUTH);
+    }
+
+    public void buyTicket() {
+        new buyTicket(this);
+    }
+
+    public void refundTicket() {
+        parent.getDbHandler().refundTicket(selectedTicket.getTicketNum());
+    }
+
 }
