3bb2f1cf39262329cf52ccf9eb757292c4883454
rtan5 <rtan5@student.ubc.ca>
2022-08-04 05:23:48 -0700

    Remove buy ticket functionality from ViewerTicketsPanel

--- src/tabs/ViewerTicketsPanel.java
+++ src/tabs/ViewerTicketsPanel.java
@@ -1,7 +1,6 @@
 package tabs;
 
 import model.Ticket;
-import popUps.BuyTicket;
 import ui.AbstractScreen;
 
 import javax.swing.*;
@@ -25,13 +24,7 @@ public class ViewerTicketsPanel extends Panel {
     private void addToolbar() {
         JPanel toolbar = new JPanel(new GridLayout(0, 1));
         AbstractScreen.setColors(toolbar, "s");
-        JButton buyTicketButton = new JButton("Buy Ticket");
-        buyTicketButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buyTicket();
-            }
-        });
+
         JButton refundTicketButton = new JButton("Refund Ticket");
         refundTicketButton.addActionListener(new ActionListener() {
             @Override
@@ -39,17 +32,10 @@ public class ViewerTicketsPanel extends Panel {
                 refundTicket();
             }
         });
-        buyTicketButton.setPreferredSize(new Dimension(SCREEN_WIDTH * 3/4, 50));
-        AbstractScreen.setColors(buyTicketButton, "s");
         refundTicketButton.setPreferredSize(new Dimension(SCREEN_WIDTH * 3/4, 50));
         AbstractScreen.setColors(refundTicketButton, "s");
-        toolbar.add(buyTicketButton);
         toolbar.add(refundTicketButton);
         panel.add(toolbar, BorderLayout.SOUTH);
-    }
-
-    public void buyTicket() {
-        new BuyTicket(this);
     }
 
     public void refundTicket() {
