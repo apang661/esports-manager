2335259c71f9f07b85e94dc4029cdf24b6653779
rtan5 <rtan5@student.ubc.ca>
2022-08-05 22:33:14 -0700

    Implement ticket panel

--- src/tabs/ViewerTicketsPanel.java
+++ src/tabs/ViewerTicketsPanel.java
@@ -4,21 +4,58 @@ import model.Ticket;
 import ui.AbstractScreen;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import static ui.AbstractScreen.SCREEN_HEIGHT;
 import static ui.AbstractScreen.SCREEN_WIDTH;
 
 public class ViewerTicketsPanel extends Panel {
 
-    private Ticket selectedTicket;
+    private int selectedTicketNum;
+    private DefaultListModel<String> tListModel;
+    private JList<Ticket> ticketList;
+    private int viewerID;
+    private ArrayList<Integer> ticketNums;
 
-    public ViewerTicketsPanel(AbstractScreen parent) {
+    public ViewerTicketsPanel(AbstractScreen parent, int viewerID) {
         super(parent);
-        // TODO: display tickets with arena/team names/seat number
-        // make selectable
+        this.viewerID = viewerID;
+        getTickets();
+        displayTickets();
         addToolbar();
+    }
+
+    private void displayTickets() {
+        ticketList = new JList(tListModel);
+        getTickets();
+        ticketList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        ticketList.setSelectedIndex(0);
+        ticketList.setVisibleRowCount(10);
+        AbstractScreen.setColors(ticketList, "s");
+        JScrollPane ticketSP = new JScrollPane(ticketList);
+        AbstractScreen.setColors(ticketSP, "s");
+        ticketSP.setPreferredSize(new Dimension( SCREEN_WIDTH * 3/4, SCREEN_HEIGHT/4));
+        ticketList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                selectedTicketNum = ticketNums.get(ticketList.getSelectedIndex());
+            }
+        });
+        panel.add(ticketSP);
+    }
+
+    public void getTickets() {
+        tListModel = new DefaultListModel<>();
+        ArrayList<String> tickets = parent.getDbHandler().getViewerTickets(viewerID);
+        for (String t : tickets) {
+            tListModel.addElement(t);
+        }
+        ticketNums = parent.getDbHandler().getViewerTicketNums(viewerID);
     }
 
     private void addToolbar() {
@@ -39,7 +76,9 @@ public class ViewerTicketsPanel extends Panel {
     }
 
     public void refundTicket() {
-        parent.getDbHandler().unbookTicket(selectedTicket.getTicketNum());
+        parent.getDbHandler().unbookTicket(selectedTicketNum);
+//        ((ViewerScreen) parent).refreshTicketPanel();
     }
+
 
 }
